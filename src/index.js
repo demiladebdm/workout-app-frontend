130727f079683523e73b410f5a3820da8b6e518e
danielmordi <danielmordi@gmail.com>
2023-03-10 15:13:49 +0100

    minor patch 2.0.1 new

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { disableReactDevTools } from '@fvilers/disable-react-devtools'
 
 // import higher order components
 import { WorkoutsContextProvider } from './context/WorkoutContext';
 import { AuthContextProvider } from './context/AuthContext';
 
-if(process.env.NODE_ENV === 'production') disableReactDevTools()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
