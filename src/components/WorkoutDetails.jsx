af00eda00750b9d04690b648f6476b7f3e2c0451
danielmordi <danielmordi@gmail.com>
2023-03-10 18:09:11 +0100

    my new pushh

--- src/components/WorkoutDetails.jsx
+++ src/components/WorkoutDetails.jsx
@@ -25,7 +25,7 @@ const WorkoutDetails = ({ workout }) => {
       return
     }
 
-    const response = await fetch('https://workout-app-api.onrender.com/api/workouts/' + workout._id, {
+    const response = await fetch('/api/workouts' + workout._id, {
       method: 'DELETE',
       headers: {
         'Authorization': `Bearer ${user.token}`
