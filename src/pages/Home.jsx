45dce98b9682a3d9bc62ff70db8984a68e2fe3e7
danielmordi <danielmordi@gmail.com>
2023-03-11 08:47:11 +0100

    with no authentication 2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
 
     useEffect(() => {
         const fetchWorkouts = async () => {
-            const response = await fetch('https://workout-app-api.onrender.com/api/workouts', {
+            const response = await fetch('/api/workouts', {
                 headers: {
                     'Authorization': `Bearer ${user.token}`
                 }
